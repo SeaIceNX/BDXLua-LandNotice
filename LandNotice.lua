d671c48beb7f8f839c16a18a76667f1df455657a
SeaIce <63960170+SeaIceNX@users.noreply.github.com>
2021-05-02 15:55:57 +0800

    Update LandNotice.lua

--- LandNotice.lua
+++ LandNotice.lua
@@ -1,8 +1,8 @@
 function SetState(name)
-    dput(name,"landin","0")
-    dput(name,"landout","1")
+    dput(name, "landin", "0")
+    dput(name, "landout", "1")
 end
-Listen("onJoin","SetState")
+Listen("onJoin", "SetState")
 
 function SendNotice()
     players = oList()
@@ -10,29 +10,28 @@ function SendNotice()
         pname = players[i]
         owner = nil
         if getLand(getPos(pname)) ~= nil then
-            kkk,kkkk,owners = getLand(getPos(pname))
+            a, b, owners = getLand(getPos(pname))
             owner = owners[1]
         end
-        if owner ~= nil and dget(pname,"landin") == "0" then
-            sendText(pname,"你进入了"..owner.."的领地",4)
-            dput(pname,"landin","1")
-            dput(pname,"landout","0")
-        end
-        if owner == nil and dget(pname,"landout") == "0" then
-            sendText(pname,"你离开了领地",4) 
-            dput(pname,"landin","0")
-            dput(pname,"landout","1")
+        if owner ~= nil and dget(pname, "landin") == "0" then
+            sendText(pname, "你进入了" .. owner .. "的领地", 4)
+            dput(pname, "landin", "1")
+            dput(pname, "landout", "0")
+        elseif owner == nil and dget(pname, "landout") == "0" then
+            sendText(pname, "你离开了领地", 4) 
+            dput(pname, "landin", "0")
+            dput(pname, "landout", "1")
         end
     end
 end
-schedule("SendNotice",1,0)
+schedule("SendNotice", 1, 0)
 
 function Fixsystem(s)
     if s ~= nil then
-        s = string.gsub(s, ',"system"','')
-        s = string.gsub(s, '","',',')
+        s = string.gsub(s, ',"system"', '')
+        s = string.gsub(s, '","', ',')
     return s
     end
 end
 
-print'LandNotice 已载入'
+print"LandNotice 已载入"
